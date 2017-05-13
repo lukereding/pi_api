6300195a23595b623af87b317ef3130ec5f67f04
luke reding <lreding@continuum.io>
2017-05-12 22:19:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response, render_template, request
 from gpiozero import CPUTemperature
 import plivo, plivoxml
+import os
 
 def send_email(message, password):
     """Send reminder emails to everyone in dict_of_recipients."""
